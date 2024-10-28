0ae67dc7ad0a7632119e5e1ca1e1820fe973363e
Bhavya <142318064+GodxD69@users.noreply.github.com>
2024-10-28 15:59:07 +0530

    Update header.tsx

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -14,8 +14,8 @@ const Header = ({ label } : HeaderProps) => {
   return (
     <div className="w-full flex flex-col gap-y-1 items-center justify-center">
         <h1 className={cn("text-5xl font-bold", font.className)}>
-            Ani
-            <span className="text-primary">Fire</span>
+            God
+            <span className="text-primary">Anime</span>
         </h1>
         <p>{label}</p>
     </div>
